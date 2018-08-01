6cd4a808a671163e1b9c2ac56aa4ffc4761cfe8d
k8tems <k8tems@live.com>
2018-08-01 20:01:22 +0900

    Add assertion

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 import unittest
+from tdidt import build
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         with open('expected.txt') as f:
             expected = f.read()
+        node_list, test_set = build('data_exercise_1.csv')
+        self.assertEqual(expected, ' '.join([str(n) for n in node_list]))
 
 
 if __name__ == '__main__':
